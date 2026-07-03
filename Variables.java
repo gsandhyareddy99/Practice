a7e1292e34e4f4fff74af3a58f732faa0b21f083
gsandhyareddy99 <gsandhyareddy99@gmail.com>
2026-07-03 15:04:12 -0500

    initial commit

--- Variables.java
+++ Variables.java
@@ -1,6 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    int x = 6, y = 6, z = 50;
+    int x = 6, y = 6, z = 50;git
     System.out.println(x + y + z);
+    hello ravi praksh
   }
 }
