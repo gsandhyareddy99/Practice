931a8cd6e212d08aecc3dd91970145ef2317df71
gsandhyareddy99 <gsandhyareddy99@gmail.com>
2026-07-03 15:31:20 -0500

    12

--- sample practice.java
+++ sample practice.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void (String[] args) {2222
         System.out.println("Hello, World+ me!");
-        
+        new line of code 
     }
 }
