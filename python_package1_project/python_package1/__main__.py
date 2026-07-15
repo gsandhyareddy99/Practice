d5aa2ba3a53761722c89207c8d0e7eeeb51dc2a4
gsandhyareddy99 <gsandhyareddy99@gmail.com>
2026-07-15 13:43:40 -0500

    Python Changes1

---
+++ python_package1_project/python_package1/__main__.py
@@ -0,0 +1,9 @@
+# TODO: Update the main function to your needs or remove it.
+
+
+def main():
+    print("Hello world!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ python_package1_project/python_package1/import_sys.py
@@ -0,0 +1,9 @@
+x = 5
+y = "Hello, World!"
+
+print(x) 
+print(y)
+print("This is a Python package.")
+
+for i in range(5):
+    print(f"Loop iteration: {i}")
